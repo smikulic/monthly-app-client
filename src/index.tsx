cafd125a5e167f0ff29f5487665a8cdb96e140df
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2022-09-03 23:02:33 +0200

    Add auth header context

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { AUTH_TOKEN } from "./constants";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:3001",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: "http://localhost:3001",
+  // uri: "http://localhost:3001",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
