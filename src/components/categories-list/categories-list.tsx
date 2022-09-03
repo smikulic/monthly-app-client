f64135412e08fecf8a6c96599f17f44dbe1bfc27
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2022-09-03 23:43:14 +0200

    Add category mutation

--- src/components/categories-list/categories-list.tsx
+++ src/components/categories-list/categories-list.tsx
@@ -1,22 +1,69 @@
-import * as React from "react";
-import { CategoryListQuery } from "../../generated/graphql";
+import { gql } from "@apollo/client";
+import React, { useState } from "react";
+import {
+  CategoryListQuery,
+  useCreateCategoryMutation,
+} from "../../generated/graphql";
 
 interface Props {
   data: CategoryListQuery;
 }
 
+export const CREATE_CATEGORY_MUTATION = gql`
+  mutation CreateCategory($name: String!) {
+    createCategory(name: $name) {
+      name
+    }
+  }
+`;
+
 export const CategoriesList: React.FC<Props> = ({ data }) => {
+  const [addCategoryField, setAddCategoryField] = useState(false);
+  const [newCategoryName, setCategoryName] = useState("");
+  const [createCategory, { data: createCategoryData, loading, error }] =
+    useCreateCategoryMutation({
+      variables: {
+        name: newCategoryName,
+      },
+      onCompleted: ({ createCategory }) => {
+        setAddCategoryField(false);
+        setCategoryName("");
+      },
+    });
+
   return (
     <>
       <div className="listContainer">
-        {!data.categories.length && (
-          <div className="listItem">Add category &#43;</div>
-        )}
         {!!data.categories &&
           data.categories.map(
             (category, key) =>
-              !!category && <li key={key}>ha !{category.name})</li>
+              !!category && (
+                <div key={key} className="listItem">
+                  {category.name}
+                </div>
+              )
           )}
+        <>
+          {addCategoryField && (
+            <div className="listItem">
+              <input
+                type="text"
+                onChange={(e) => setCategoryName(e.target.value)}
+              />
+              <button onClick={() => createCategory()}>
+                {loading ? "saving..." : "Add"}
+              </button>
+            </div>
+          )}
+          {!addCategoryField && (
+            <div
+              className="listItem add"
+              onClick={() => setAddCategoryField(true)}
+            >
+              Add category &#43;
+            </div>
+          )}
+        </>
       </div>
     </>
   );
--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -106,6 +106,13 @@ export type CategoryListQueryVariables = Exact<{ [key: string]: never; }>;
 
 export type CategoryListQuery = { __typename?: 'Query', categories: Array<{ __typename?: 'Category', name: string }> };
 
+export type CreateCategoryMutationVariables = Exact<{
+  name: Scalars['String'];
+}>;
+
+
+export type CreateCategoryMutation = { __typename?: 'Mutation', createCategory: { __typename?: 'Category', name: string } };
+
 export type SignupMutationVariables = Exact<{
   email: Scalars['String'];
   password: Scalars['String'];
@@ -157,6 +164,39 @@ export function useCategoryListLazyQuery(baseOptions?: Apollo.LazyQueryHookOptio
 export type CategoryListQueryHookResult = ReturnType<typeof useCategoryListQuery>;
 export type CategoryListLazyQueryHookResult = ReturnType<typeof useCategoryListLazyQuery>;
 export type CategoryListQueryResult = Apollo.QueryResult<CategoryListQuery, CategoryListQueryVariables>;
+export const CreateCategoryDocument = gql`
+    mutation CreateCategory($name: String!) {
+  createCategory(name: $name) {
+    name
+  }
+}
+    `;
+export type CreateCategoryMutationFn = Apollo.MutationFunction<CreateCategoryMutation, CreateCategoryMutationVariables>;
+
+/**
+ * __useCreateCategoryMutation__
+ *
+ * To run a mutation, you first call `useCreateCategoryMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateCategoryMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createCategoryMutation, { data, loading, error }] = useCreateCategoryMutation({
+ *   variables: {
+ *      name: // value for 'name'
+ *   },
+ * });
+ */
+export function useCreateCategoryMutation(baseOptions?: Apollo.MutationHookOptions<CreateCategoryMutation, CreateCategoryMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateCategoryMutation, CreateCategoryMutationVariables>(CreateCategoryDocument, options);
+      }
+export type CreateCategoryMutationHookResult = ReturnType<typeof useCreateCategoryMutation>;
+export type CreateCategoryMutationResult = Apollo.MutationResult<CreateCategoryMutation>;
+export type CreateCategoryMutationOptions = Apollo.BaseMutationOptions<CreateCategoryMutation, CreateCategoryMutationVariables>;
 export const SignupDocument = gql`
     mutation Signup($email: String!, $password: String!) {
   signup(email: $email, password: $password) {
