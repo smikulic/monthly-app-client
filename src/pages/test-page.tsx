382209fc7edeb407d542d4690fc22ed874965ee9
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2023-11-09 15:23:10 +0100

    better stack

---
+++ src/pages/test-page.tsx
@@ -0,0 +1,339 @@
+import React from "react";
+import "./test-page.css";
+
+export const TestPage = () => {
+  return (
+    <div className="container">
+      <div className="main">
+        <div className="header margin-top-40">
+          <div className="logo-container">
+            <div className="logo">wallmine</div>
+            <div className="text"> - status</div>
+          </div>
+          <div className="cta">E-mail helpdesk</div>
+        </div>
+
+        <div className="title margin-top-40">
+          <div className="status-icon">icon</div>
+          <h2 className="status-text">All services are online</h2>
+          <p className="status-description">As of June 1st, 1:46 PM ET</p>
+        </div>
+
+        <div className="status margin-top-40">
+          <div className="header">
+            <h4>Current status by service</h4>
+            <div>indicator icons</div>
+          </div>
+
+          <div className="service">
+            <div className="service-header">
+              <div>API</div>
+              <div>100% uptime</div>
+            </div>
+            <div className="service-uptime">
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+            </div>
+          </div>
+
+          <div className="service">
+            <div className="service-header">
+              <div>Dashboard</div>
+              <div>99.97% uptime</div>
+            </div>
+            <div className="service-uptime">
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+            </div>
+          </div>
+          <div className="service">
+            <div className="service-header">
+              <div>Landing Page</div>
+              <div>100% uptime</div>
+            </div>
+            <div className="service-uptime">
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+              <span className="service-uptime-tick"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
