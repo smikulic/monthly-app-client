7fa4f4d4eba1a05fc71e700c9ca0fa396e52a752
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2023-11-19 04:28:55 +0100

    Fix format currency utility

--- src/utils/format.test.ts
+++ src/utils/format.test.ts
@@ -2,18 +2,28 @@
 import { formatAmount } from "./format";
 
 describe("formatAmount", () => {
-  it("should format 100 euro", async () => {
+  it("should format 100 EUR", async () => {
     const formattedAmount = formatAmount(100);
-    expect(formattedAmount).toBe("100 €");
+    expect(formattedAmount).toBe("100 €");
   });
 
-  it("should format 1_000 euro", async () => {
+  it("should format 1_000 EUR", async () => {
     const formattedAmount = formatAmount(1_000);
-    expect(formattedAmount).toBe("1,000 €");
+    expect(formattedAmount).toBe("1.000 €");
   });
-  
-  it("should format 10_000 euro", async () => {
+
+  it("should format 10_000 EUR", async () => {
     const formattedAmount = formatAmount(10_000);
-    expect(formattedAmount).toBe("10,000 €");
+    expect(formattedAmount).toBe("10.000 €");
+  });
+
+  it("should format 100 USD", async () => {
+    const formattedAmount = formatAmount(100, "USD");
+    expect(formattedAmount).toBe("$100");
+  });
+
+  it("should format 1_000 USD", async () => {
+    const formattedAmount = formatAmount(1_000, "USD");
+    expect(formattedAmount).toBe("$1,000");
   });
 });
--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,15 +1,17 @@
 // import accounting from "accounting";
 
 export const formatAmount = (amount: number, userCurrency?: string): string => {
-  let intlCurrency = Intl.NumberFormat("en-DE", {
+  let intlCurrency = Intl.NumberFormat("de-DE", {
     style: "currency",
     currency: "EUR",
+    maximumFractionDigits: 0,
   });
 
   if (userCurrency === "USD") {
     intlCurrency = new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
+      maximumFractionDigits: 0,
     });
   }
 
@@ -17,6 +19,7 @@ export const formatAmount = (amount: number, userCurrency?: string): string => {
     intlCurrency = Intl.NumberFormat("en-GB", {
       style: "currency",
       currency: "GBP",
+      maximumFractionDigits: 0,
     });
   }
 
@@ -24,6 +27,7 @@ export const formatAmount = (amount: number, userCurrency?: string): string => {
     intlCurrency = Intl.NumberFormat("en-CA", {
       style: "currency",
       currency: "CAD",
+      maximumFractionDigits: 0,
     });
   }
 
