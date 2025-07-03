0f87305d4279d29e302ac5613d5d57ab51ad6a42
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2025-07-03 14:57:02 +0200

    tech: fix env variable

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const AUTH_TOKEN = import.meta.env.VITE_AUTH_TOKEN_KEY;
 export const AUTH_TOKEN_USER = import.meta.env.VITE_AUTH_TOKEN_USER_KEY;
 export const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
-export const API_PRODUCTION = import.meta.env.API_PRODUCTION;
+export const API_PRODUCTION = import.meta.env.VITE_API_PRODUCTION;
 
 export const months = [
   "Jan",
