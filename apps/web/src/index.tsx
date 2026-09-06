1b2a50abe58e7d7e50002bfeabc964ac4a72710b
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2026-09-06 22:37:42 +0200

    feat: minor cleanup

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -10,7 +10,6 @@ import * as Sentry from "@sentry/browser";
 import "./index.css";
 import "react-toastify/dist/ReactToastify.min.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { API_PRODUCTION, AUTH_TOKEN, SENTRY_DSN } from "./constants";
 import { analytics } from "./utils/mixpanel";
 import "@fontsource/roboto/300.css";
@@ -76,7 +75,5 @@ root.render(
   </ApolloProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+// Web vitals are not reported anywhere. `reportWebVitals` is still here if that
+// changes; give it a reporter that sends somewhere rather than console.log.
